7f418942f4af37445f45bb5f95a26177732a2c1a
nithular <nithularatnayaker@gmail.com>
2023-03-17 13:51:30 +0800

    Model Prototypes

---
+++ core/tempmodel.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Retailer(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+
+class Location(models.Model):
+    latlon = models.DecimalField(max_digits=9, decimal_places=6)
+    address = models.TextField()
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    retailerID = models.ForeignKey(Retailer)
+    location = models.OneToOneField(Location)
+    product = models.ManyToManyField(Product)
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.TextField()
+
+class Watchlist(models.Model):
+    name = models.CharField(max_length=100)
+    itemCount = models.IntegerField()
+    userID = models.ForeignKey(User)
+    watchlistProduct = models.ManyToManyField(Product)
+
+class UserLocation(models.Model):
+    #class Meta:
+    #    unique_together = (('userID', 'locationID'),)
+    
+    userID = models.ForeignKey(User)
+    locationID = models.ForeignKey(Location)
