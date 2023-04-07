13c7d321c1dcfaa88cb3388515e69cd75781d2e6
Jehan Hapugale <jehan.hapugalle@gmail.com>
2023-04-08 00:31:05 +0800

    Watchlist GET and POST functions

--- core/views.py
+++ core/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import *
+from .serializers import *
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 def home(request):
     return render(request, 'index.html')
@@ -8,10 +14,22 @@ def profile(request):
 
 def search(request):
     return render(request, 'search.html')
-
+    
+@api_view(['GET', 'POST'])
 def watchlists(request):
-    return render(request, 'watchlists.html')
+    # return render(request, 'watchlists.html')
+    
+    if request.method == 'GET': 
+        wlists = Watchlist.objects.all()
+        serializer = WatchlistSerializer(wlists, many=True)
+        return JsonResponse(serializer.data, safe=False)
 
+    if request.method == 'POST': 
+        serializer = WatchlistSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
 def watchlistdetail(request):
     return render(request, 'watchlistdetail.html')
 def login(request):
