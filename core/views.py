87e44e2ea8042c9a0dd91d92e9d729221b800707
Jehan Hapugale <jehan.hapugalle@gmail.com>
2023-04-08 12:42:39 +0800

    Watchlist update (PUT) function

--- core/views.py
+++ core/views.py
@@ -42,9 +42,15 @@ def watchlistdetail(request, id):
     if request.method == 'GET':
         serializer = WatchlistSerializer(watchlist)
         return Response(serializer.data)
-    elif request.method == 'POST':
-        pass
+    
     elif request.method == 'PUT':
+        serializer = WatchlistSerializer(watchlist, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+    elif request.method == 'DELETE':
         pass
 
 def login(request):
