0aece071493e04def5fea99d001fbb00174a85a0
Shey Laplanche <shey.laplanche@student.curtin.edu.au>
2023-04-22 12:52:54 +0800

    Added notes to update_db for devs to continue WIP

--- core/management/commands/update_db.py
+++ core/management/commands/update_db.py
@@ -9,5 +9,20 @@ class Command(BaseCommand):
         # Gather latest products
         scraper = woolworths.WoolworthsScraper()
         products = scraper.scrape_all_products()
+        print(products)
+        """
+        This is the format of the products variable (dict):
+        {
+            'Fruit & Veg': [
+                ['663734', 'The Salad Servers Turmeric Cauliflower & Cranberry Salad 300g', '$6.30'], 
+                ['839321', 'The Salad Servers Salad Bean Medley 300g', '$6.30'], 
+                ['11358', 'The Salad Servers Lentil Salad 800g', '$10.50'],
+                ...
+            ]
+        }
+        """
+
 
         # Unpack data and insert into database
+
+        # ... here is the code that will insert shit into the db
