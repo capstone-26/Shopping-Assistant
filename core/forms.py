cdd4c5f194f33e149aade75acc3c7d59f3602eac
nithular <nithularatnayaker@gmail.com>
2023-05-09 14:49:43 +0800

    Created Edit UserProfile and Edit User form

---
+++ core/forms.py
@@ -0,0 +1,13 @@
+from .models import *
+from django import forms
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio', 'location')
